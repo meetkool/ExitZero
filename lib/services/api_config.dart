8b062eaa28d85768ef630442ea069bca7d2c99b0
meetkool <iitjeemeet@gmail.com>
2026-02-13 02:47:46 +0530

    just push it

--- lib/services/api_config.dart
+++ lib/services/api_config.dart
@@ -3,8 +3,8 @@ class ApiConfig {
   ApiConfig._();
 
   /// Base URL of the Bizniz backend.
-  // static const String baseUrl = 'https://biznuz.mockpeer.me/api/v1';
-  static const String baseUrl = 'https://backend_test.mockpeer.me/api/v1';
+  static const String baseUrl = 'https://biznuz.mockpeer.me/api/v1';
+  // static const String baseUrl = 'https://backend_test.mockpeer.me/api/v1';
   // static const String baseUrl = 'http://localhost:8000/api/v1';
 
 
